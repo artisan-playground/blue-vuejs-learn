3bb737ed2d8303032f404f29c2cc9d0e3d088b83
BlueAlis <nattaphatblue.2545@gmail.com>
2020-10-14 17:28:10 +0700

    tset

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to BlueAlisasz Vue.js App" />
+    <HelloWorld msg="Welcome to BlueAlisa Vue.js App" />
   </div>
 </template>
 
